00b2639cdfc7c191b4594239f7151ff8e1e71041
sameer <sameer.8t1@gmail.com>
2026-09-11 19:15:26 +0530

    Change student name from 'Yash' to 'Sameer'

--- Assignment/Assignment 02/Q3.js
+++ Assignment/Assignment 02/Q3.js
@@ -5,7 +5,7 @@ const server = http.createServer((req, res) => {
     if (req.url === '/student') {
         const student = {
             id: 101,
-            name: "Yash",
+            name: "Sameer",
             course: "B.tech",
             semester: 3,
             city: "Mumbai"
