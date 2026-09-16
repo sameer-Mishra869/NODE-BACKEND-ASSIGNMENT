720362cc71a81a556e46cc90d6e002c23d84e890
sameer <sameer.8t1@gmail.com>
2026-09-16 13:52:18 +0530

    Delete Assignment 11 directory

--- Assignment 11/model/studentModel.js
+++
@@ -1,4 +0,0 @@
-const mongoose = require('mongoose');
-const studentSchema = require('../schema/studentSchema');
-
-module.exports = mongoose.model('Assignment11Student', studentSchema);
--- Assignment 11/model/teacherModel.js
+++
@@ -1,4 +0,0 @@
-const mongoose = require('mongoose');
-const teacherSchema = require('../schema/teacherSchema');
-
-module.exports = mongoose.model('Assignment11Teacher', teacherSchema);
